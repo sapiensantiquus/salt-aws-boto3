0acbe6b39eeca2daa58d8ed443b8700d962fb766
Megan <megan.lkm@gmail.com>
2017-10-07 14:03:58 -0500

    simplify ecs

--- aws_boto3/__init__.py
+++ aws_boto3/__init__.py
@@ -1,5 +1,6 @@
 from aws_boto3.dynamodb import ddb_get_table
 from aws_boto3.ecr import ecr_absent_repo, ecr_ensure_repo
+from aws_boto3.ecs import ecs_ensure_cluster, ecs_ensure_register_task, ecs_ensure_service
 from aws_boto3.iam import iam_ensure_role
 from aws_boto3.kms import kms_ensure_key
 from aws_boto3.lambdas import lambda_sync_function
--- aws_boto3/ecs/tasks.py
+++
@@ -1,6 +0,0 @@
-from aws_boto3.common import get_client
-
-
-def ecs_ensure_register_task(task, region=None):
-    client = get_client('ecs', region=region)
-    return client.register_task_definition(**task['task_definition'])
