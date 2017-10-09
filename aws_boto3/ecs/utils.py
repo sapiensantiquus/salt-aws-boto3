1869a60e922027363e31ace50d3a7b3656335a0e
Megan <megan.lkm@gmail.com>
2017-10-09 14:22:41 -0500

    split ecs

--- aws_boto3/ecs/utils.py
+++ aws_boto3/ecs/utils.py
@@ -46,17 +46,5 @@ def ecs_ensure_service(service, region=None, client=None):
 
 @boto_client('ecs')
 def ecs_ensure_cluster(cluster, region=None, client=None):
-    cluster_definition = cluster['cluster_definition']
-    response = client.create_cluster(**cluster_definition)
-
-    cluster_arn = response['cluster']['clusterArn']
-    service_response = None
-    if 'services' in cluster:
-        services = cluster['services']
-        response['services'] = []
-        for service in services:
-            service['service_definition']['cluster'] = cluster_arn
-            service_response = ecs_ensure_service(service, region)
-            response['services'].append(service_response)
-
-    return response
+    response = client.create_cluster(**cluster)
+    return response['cluster']['clusterArn']
