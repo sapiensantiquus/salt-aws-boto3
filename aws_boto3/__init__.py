2b0bc74e5e4ad67086e1a2e3d8052719de57aa32
Megan <megan.lkm@gmail.com>
2017-10-16 14:34:45 -0500

    lambda invoke function

--- aws_boto3/__init__.py
+++ aws_boto3/__init__.py
@@ -1,7 +1,8 @@
+# required by SaltStack
 from aws_boto3.dynamodb import ddb_get_table
 from aws_boto3.ecr import ecr_absent_repo, ecr_ensure_repo
 from aws_boto3.ecs import ecs_ensure_config
 from aws_boto3.iam import iam_ensure_role
 from aws_boto3.kms import kms_ensure_key
-from aws_boto3.lambdas import lambda_sync_function
+from aws_boto3.lambdas import lambda_invoke, lambda_sync_function
 from aws_boto3.s3 import s3_ensure_bucket
