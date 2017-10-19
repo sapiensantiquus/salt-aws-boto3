9611f8873b76b9850127f16e6bc29b6319ed56ac
Andrew <ajohnson@mgage.com>
2017-10-19 10:06:00 -0500

    ELB

---
+++ aws_boto3/ec2/elb.py
@@ -0,0 +1,8 @@
+from aws_boto3.common import boto_client
+from botocore.exceptions import ClientError
+
+
+@boto_client('elb')
+def ensure_elb(elb_def, region=None, client=None):
+    response = client.create_load_balancer(**elb_def)
+    return response['DNSName']
